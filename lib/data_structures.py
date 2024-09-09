b6563921269bf02155faf8e54a119464db9cdf3b
Ngecha <ngechadennis@gmail.com>
2024-09-09 14:08:04 +0300

    changes

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,7 +17,7 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    return(food["name"] for food in spicy_foods)
+    return[food["name"] for food in spicy_foods]
 
 
 def get_spiciest_foods(spicy_foods):
@@ -36,7 +36,7 @@ print_spicy_foods(spicy_foods)
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
       for food in spicy_foods:
         if food['cuisine'] == cuisine:
-            print(food)
+            return(food)
       return None
 
 get_spicy_food_by_cuisine(spicy_foods,"American")
@@ -47,7 +47,7 @@ def print_spiciest_foods(spicy_foods):
                  name = food['name']
                  cuisine = food['cuisine']
                  heat_level = "\U0001F336" * food["heat_level"]
-                 print([f"{name} ({cuisine}) | Heat level: {heat_level}"])
+                 print(f"{name} ({cuisine}) | Heat Level: {heat_level}")
 
                 
 print_spiciest_foods(spicy_foods)
